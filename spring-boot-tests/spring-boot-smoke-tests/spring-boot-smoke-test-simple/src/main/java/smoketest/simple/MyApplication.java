6b4da598d064eb1f09cb4e7c5feecce714a5feef
liuwenqiang <Aa123456>
2024-08-06 16:18:02 +0800

    第一次启动源码，删除了一些kafka的自动装配类，因为用不到而且报错我没找到原因，估计是和gradle仓库有关系。 在测试类中加入了web模块，用来启动主类保证其不退出

---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-simple/src/main/java/smoketest/simple/MyApplication.java
@@ -0,0 +1,12 @@
+package smoketest.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+		System.out.println("SpringBoot源码");
+	}
+}
